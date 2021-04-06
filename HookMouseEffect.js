abaed72dce1b4dcc7bd0e8de3aa98836d4084b4c
Kondwani Ngulube <47284906+Kondwani7@users.noreply.github.com>
2021-04-06 19:09:24 +0200

    Add files via upload
    useEffect to track the mouse position

---
+++ HookMouseEffect.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react'
+
+function HookMouseEfect() {
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+    const logMousePosition= e=>{
+        console.log('Mouse position event')
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+
+    useEffect(()=>{
+        console.log('useEffect called')
+        window.addEventListener('mouseover', logMousePosition)
+
+        return()=>{
+            console.log('component umounted')
+            window.removeEventListener(logMousePosition)
+        }
+    },[])
+
+    return (
+        <div>
+            Hooks X - {x} Y - {y}
+        </div>
+    )
+}
+
+export default HookMouseEfect
